42843e7e8f3f115cacd380ee752c41ba69544e34
Aleksandr Shenshin <shenshin@me.com>
2021-02-07 20:30:57 +0100

    remove inline styling from every component

--- src/components/SpotifyWidget/Playlist/Playlist.tsx
+++ src/components/SpotifyWidget/Playlist/Playlist.tsx
@@ -21,17 +21,13 @@ const Playlist = ({ playlist }: { playlist: PlaylistType }) => {
         onKeyDown={() => {}}
         role="button"
         tabIndex={-1}
-        style={{
-          cursor: 'pointer',
-          display: 'flex',
-          alignItems: 'center',
-        }}
+        className={style.bigButton}
       >
-        <div style={{ width: '6rem', marginBottom: '0.5rem' }}>
-          <h3 style={{ width: 'max-content' }}>{`${items.length} tracks`}</h3>
-          <img style={{ width: '100%' }} src={imageURL} alt={description} />
+        <div>
+          <h3>{`${items.length} tracks`}</h3>
+          <img src={imageURL} alt={description} />
         </div>
-        <h1 style={{ margin: 'auto' }}>{name}</h1>
+        <h1>{name}</h1>
       </div>
       {tracksVisible && <Tracklist tracks={items.map((item) => item.track)} />}
     </div>
