343b0896c417a1e702e276e233fcb8623954bdea
Aleksandr Shenshin <shenshin@me.com>
2021-02-07 01:23:54 +0100

    make token a state variable

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,11 @@ const App = () => {
     setTheme(isDark ? 'dark' : 'light');
   };
 
-  const playLists = [
-    '37i9dQZF1DWXRqgorJj26U',
-    '37i9dQZF1DWWGFQLoP9qlv',
-    '37i9dQZEVXbKCF6dqVpDkS',
-  ];
   return (
     <div className={theme}>
       <ThemeSwitch theme={theme} switchTheme={switchTheme} />
       <h1>Spotify Widget</h1>
-      <Widget playListIDs={playLists} />
+      <Widget />
     </div>
   );
 };
--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect, useCallback } from 'react';
 import style from './Widget.module.scss';
 import Playlist from './Playlist/Playlist';
 
-const Widget = ({ playListIDs }: { playListIDs: string[] }) => {
+const Widget = () => {
   const [error, setError] = useState('');
+  const [token, setToken] = useState('');
   const [playlists, setPlaylists] = useState(Array);
 
-  const retrieveToken = async () => {
+  async function retrieveToken() {
     const response = await fetch('https://accounts.spotify.com/api/token', {
       method: 'post',
       headers: {
@@ -17,33 +18,37 @@ const Widget = ({ playListIDs }: { playListIDs: string[] }) => {
     });
     if (!response.ok) throw new Error(`Error retrieving token: ${response.statusText}`);
     const tokenData = await response.json();
-    return tokenData.access_token;
-  };
+    setToken(tokenData.access_token);
+  }
 
   const retrievePlaylist = useCallback(async (playlist: string) => {
     const response = await fetch(`https://api.spotify.com/v1/playlists/${playlist}`, {
       method: 'get',
       headers: {
-        Authorization: `Bearer ${await retrieveToken()}`,
+        Authorization: `Bearer ${token}`,
       },
     });
     if (!response.ok) throw new Error(`Error retrieving playlist: ${response.statusText}`);
     return response.json();
-  }, []);
-
-  const getPlaylistData = useCallback(async () => {
-    try {
-      setError('');
-      const playlistData = await Promise.all(playListIDs.map((id) => retrievePlaylist(id)));
-      setPlaylists(playlistData);
-    } catch (err) {
-      setError(err.message);
-    }
-  }, [playListIDs, retrievePlaylist]);
+  }, [token]);
 
   useEffect(() => {
-    getPlaylistData();
-  }, [getPlaylistData]);
+    (async () => {
+      const playListIDs = [
+        '37i9dQZF1DWXRqgorJj26U',
+        '37i9dQZF1DWWGFQLoP9qlv',
+        '37i9dQZEVXbKCF6dqVpDkS',
+      ];
+      try {
+        setError('');
+        await retrieveToken();
+        const playlistData = await Promise.all(playListIDs.map((id) => retrievePlaylist(id)));
+        setPlaylists(playlistData);
+      } catch (err) {
+        setError(err.message);
+      }
+    })();
+  }, [retrievePlaylist]);
 
   return (
     <div className={style.widget}>
